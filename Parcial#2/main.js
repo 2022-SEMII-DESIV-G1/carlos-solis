4a44717846a8ecbcaab871e75fafa780b1482409
Carlos Ivan Solis Donos <solisdonoso@gmail.com>
2022-10-31 19:58:19 -0500

    Parcial #2

--- Parcial#2/main.js
+++ Parcial#2/main.js
@@ -14,7 +14,7 @@ let array = [
   [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
   [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
   [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-  [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 10200],
+  [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
 ];
 
 // //? Funcion que calcula la suma mayor
@@ -54,16 +54,13 @@ const textSum = document.getElementById("sum");
 textSum.innerHTML = "La suma de la ruta mayor es de: " + maxSum(array);
 
 function maxPathSum(tri, m, n) {
-  // loop for bottom-up calculation
   let pos = [];
   let auxJ;
   for (let i = m - 1; i >= 0; i--) {
     for (let j = 0; j <= i; j++) {
       let aux = false;
-      // for each element, check both
-      // elements just below the number
-      // and below right to the number
-      // add the maximum of them to it
+      console.log("[ " + i + " ],[ " + j + " ]");
+
       if (tri[i + 1][j] > tri[i + 1][j + 1]) {
         tri[i][j] += tri[i + 1][j];
         for (let row = 0; row < pos.length; row++) {
@@ -94,8 +91,6 @@ function maxPathSum(tri, m, n) {
   }
   console.log(tri[0][0]);
 
-  // return the top element
-  // which stores the maximum sum
   return pos;
 }
 
